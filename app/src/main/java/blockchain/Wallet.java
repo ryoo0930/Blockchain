bc5aa09981dce094b9b1756cb57f3edb3016388f
ryoo0930 <ryoo0930@naver.com>
2025-11-07 10:49:19 +0900

    KeyPair을 저장하는 Wallet 클래스 생성

---
+++ app/src/main/java/blockchain/Wallet.java
@@ -0,0 +1,36 @@
+package blockchain;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.ECGenParameterSpec;
+
+import javax.management.RuntimeErrorException;
+
+public class Wallet {
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public Wallet() {
+        generateKeyPair();
+    }
+
+    private void generateKeyPair() {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256r1");
+            kpg.initialize(ecSpec, new SecureRandom());
+
+            KeyPair keyPair = kpg.generateKeyPair();
+            this.privateKey = keyPair.getPrivate();
+            this.publicKey = keyPair.getPublic();
+        } catch (Exception e) {
+            throw new RuntimeException("Generate Key Pair Failed.", e);
+        }
+    }
+
+    public PrivateKey getPrivateKey() { return privateKey; }
+    public PublicKey getPublicKey() { return publicKey; }
+}
